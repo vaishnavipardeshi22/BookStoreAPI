3b1aa85ccdeb247cb57f63350cc6a4829083b6d1
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-21 21:41:19 +0530

    ADDED:Dependency For Swagger

--- src/main/java/com/bridgelabz/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/bridgelabz/bookstore/service/BookServiceImpl.java
@@ -8,11 +8,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class BookService {
+public class BookServiceImpl implements IBookService{
 
     @Autowired
     BookRepository bookRepository;
 
+    @Override
     public List<Book> getBookList() {
         return bookRepository.findAll();
     }
