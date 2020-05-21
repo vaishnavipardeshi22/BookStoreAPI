65474b2de9529855e9a6dc20d669a5fb9e064e9f
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-05-21 21:51:38 +0530

    ADDED:Interface For Book Service

---
+++ src/main/java/com/bridgelabz/bookstore/service/IBookService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.bookstore.service;
+
+import com.bridgelabz.bookstore.model.Book;
+
+import java.util.List;
+
+public interface IBookService {
+    List<Book> getBookList();
+}
